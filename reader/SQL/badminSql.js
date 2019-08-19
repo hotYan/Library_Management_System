a13a6ef278d8c26fdb92637e0661135e296e9ba4
hotyan <hotyann@163.com>
2019-08-19 17:35:33 +0800

    readme

--- reader/SQL/badminSql.js
+++
@@ -1,6 +0,0 @@
-//对后台管理员对自己信息的修改
-var badmin = {
-	update : 'update systemadmin set BAPassword=?,BAPhone=?,BAEmail=? where BAId=?'
-};
- 
-module.exports = badmin;
--- reader/models/xlsx.js
+++
@@ -1,9 +0,0 @@
-var form = formidable.IncomingForm();
-	form.encoding = 'utf-8';//设置编辑  
-	form.uploadDir = "public/avatar/";//设置文件存储路径  
-	form.keepExtensions = true;//保留后缀        
-	form.maxFieldsSize = 2 * 1024 * 1024;//设置单文件大小限制 
-	//form.maxFields = 1000;  设置所以文件的大小总和
-
-
-module.exports = pool;  
